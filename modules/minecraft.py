bc0661610b490a63fea50a32ef3d6d8778094c68
nullptr <57066793+weifanxiang@users.noreply.github.com>
2021-12-22 15:36:51 +0800

    Update minecraft.py
    在机房 未测试

--- modules/minecraft.py
+++ modules/minecraft.py
@@ -1,13 +1,14 @@
 from graia.saya import Saya, Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
 from loguru import logger
-
+from pathlib import Path
 from graia.ariadne.message.element import Plain
 from graia.ariadne.model import Group, Member
 from graia.ariadne.app import Ariadne
 from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.element import Plain, At
+from typing import Dict, Literal, Optional, Type, Tuple, Union
 import subprocess
 import os
 
@@ -17,94 +18,57 @@ channel.name(__name__)
 
 
 class Minecraft:
-    run_status = False
-    player_list = []
-    save_list = []
-    now_save = ""
+    class Server:
+        path: Path
+        name: str
+        players: list
+        lock_file: Path
 
+        def __init__(self, **kwargs):
+            self.path = kwargs["path"]
+            self.name = kwargs["name"]
+            self.lock_file = self.path / "session.lock"
+
+        async def start_server(self) -> bool:
+            if self.status:
+                return False
+            try:
+                subprocess.run(["tmux", "new", "-s", "minecraft_{self.name}", "./start.sh", "ENTER"], cwd=self.path)
+            finally:
+                self.lock_file.write_text("running")
+                return True
+
+        async def stop_server(self) -> bool:
+            if not self.status:
+                return False
+            try:
+                subprocess.run(["tmux", "send", "-t", "minecraft", "stop", "ENTER"])
+            finally:
+                self.lock_file.unlink()
+                return True
+
+        @property
+        def status(self) -> bool:
+            return self.lock_file.exists
+
+        class Config:
+            hardcore: bool = False
+            pvp: bool = True
+            enable_command_block: bool = False
+            difficulty: Literal["hard", "normal", "easy", "peaceful"]
+            async def load_config(self):
+                pass
+    
+
+    servers_dir: Path
+    servers: Dict[str, Server]
     def __init__(
         self, config: dict = {"mc_dir": "~/minecraft/", "default": "minecraft"}
     ):
-        self.run_status = True
-        self.config = config
-        self.save_list = os.listdir(config["mc_dir"])
-        self.now_save = config["default"]
-        subprocess.run(
-            [
-                "tmux",
-                "send",
-                "-t",
-                "minecraft",
-                "cd ../{}".format(config["default"]),
-                "ENTER",
-            ]
-        )
-        subprocess.run(["tmux", "send", "-t", "minecraft", "./start.sh", "ENTER"])
+        self.server_dir = Path(config["mc_dir"])
+        for i in self.servers_dir.iterdir():
+            self.servers.update(i.name, self.Server({"name":i.name, "path" :i}))
 
-    async def start_server(self):
-        if self.run_status:
-            return False
-        try:
-            subprocess.run(["tmux", "send", "-t", "minecraft", "./start.sh", "ENTER"])
-        finally:
-            self.run_status = True
-            return True
-
-    async def stop_server(self):
-        if not self.run_status:
-            return False
-        try:
-            subprocess.run(["tmux", "send", "-t", "minecraft", "stop", "ENTER"])
-        finally:
-            self.run_status = False
-            return True
-
-    async def switch(self, save: str):
-        if save in self.save_list:
-            try:
-                subprocess.run(
-                    [
-                        "tmux",
-                        "send",
-                        "-t",
-                        "minecraft",
-                        "cd ../{}".format(save),
-                        "ENTER",
-                    ]
-                )
-            except:
-                return "切换存档失败！"
-            finally:
-                self.now_save = save
-                return "切换成功！"
-
-        return "存档不存在！"
-
-    async def save(self, operator: str) -> str:
-
-        if operator == "list":
-            result = "当前服务器有：\n"
-            result += "\n".join(self.save_list)
-            logger.debug(result)
-            return result
-        elif operator.startswith("switch"):
-            if self.run_status:
-                return "服务器正在运行！请先关闭服务器！"
-            return await self.switch(operator[7:])
-        return "命令错误"
-
-    async def judge(self, message: str) -> str:
-        if message == "start":
-            return "已开启" if await self.start_server() else "启动失败"
-        elif message == "stop":
-            await self.stop_server()
-            return "已关闭" if await self.stop_server() else "关闭失败"
-        elif message == "status":
-            return "当前服务器开启状态为：" + str(self.run_status)
-        elif message.startswith("saves "):
-            return await self.save(message[6:])
-        else:
-            return "服务器暂时不支持这个命令"
 
 
 minecraft = Minecraft(config={"mc_dir": "/opt/minecraft/", "default": "hardcores"})
@@ -120,7 +84,17 @@ async def event_receiver(
             MessageChain.create(
                 [
                     At(sender.id),
-                    Plain(await minecraft.judge(message.asDisplay()[4:].strip())),
+                    Plain(await judge(message.asDisplay()[4:].strip())),
                 ]
             ),
         )
+
+async def judge(self, message: str) -> str:
+    if message.startswith("start"):
+        return "已开启" if await minecraft.servers[message[6:]].start_server() else "启动失败"
+    elif message == "stop":
+        return "已关闭" if await minecraft.servers[message[5:]].stop_server() else "关闭失败"
+    elif message == "status":
+        return "该服务器开启状态为：" + await minecraft.servers[message[5:]].status
+    else:
+        return "服务器暂时不支持这个命令"
